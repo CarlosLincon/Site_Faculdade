0557a468db1565e22c54bcb394a2f46b8f0bd9d8
CarlosLincon <carloslincon29@gmail.com>
2022-09-24 19:39:02 -0300

    Atualizando as Dependencias do projeto

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,28 +1,21 @@
 import { useState } from "react";
 import ISlide from "../../interfaces/ISlide";
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import { padding } from "@mui/system";
 import styles from "./Slider.module.css"
+import classNames from "classnames";
 interface Props {
 
   slider: ISlide[];
 }
 
 
-
 const Slider = ({ slider }: Props) => {
 
   const [indexAtual, setIndexAtual] = useState(0);
-  const goToPrevious = () => {
-    const isFirstSlide = indexAtual === 0;
-    const newIndex = isFirstSlide ? slider.length - 1 : indexAtual - 1;
-    setIndexAtual(newIndex);
-  };
+
+
 
   const goToSlide = (slideIndex: number) => {
-    console.log(slideIndex);
+
     setIndexAtual(slideIndex);
   };
 
@@ -40,7 +33,11 @@ const Slider = ({ slider }: Props) => {
       </div>
       <div className={styles.container_slider_opcao_slide}>
         {slider.map((slide, index) => (
-          <div className={styles.div_opcao_slide}
+          <div className={classNames({
+            [styles.div_opcao_slide]: true,
+            [styles.div_opcao_slide_ativo]: indexAtual === index,
+
+          })}
             key={index}
             onClick={() => goToSlide(index)}
           >
